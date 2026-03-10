00ee63ff54a6aabbfa86667dc45e546776bb3a15
Diego Santos <santos.diegoj86@gmail.com>
2026-03-10 16:50:10 -0300

    feat(network): add internet gateway, route table, and public route for VPC

--- network.tf
+++ network.tf
@@ -26,3 +26,33 @@ resource "aws_subnet" "public" {
     Owner       = var.owner
   }
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "p02-dev-igw"
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Owner       = var.owner
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "p02-dev-public-rt"
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Owner       = var.owner
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
